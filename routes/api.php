3ce95efe42280434a7da20493e10d09c3cc9f757
omarkhalil117 <omarkhalil117@gmail.com>
2024-06-20 04:42:08 +0300

    add doctors endpoint with filters

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use \App\Http\Controllers\API\DoctorController;
+use \App\Models\Doctor;
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
@@ -10,3 +13,31 @@ Route::get('hospitals/{hospital}/applications', [\App\Http\Controllers\API\Inten
 Route::put('/applications/{application}', [\App\Http\Controllers\API\IntensiveCareApplicationController::class, 'updateStatus']);
 Route::apiResource('/intensive-care-units', \App\Http\Controllers\API\IntensiveCareUnitController::class);
 Route::apiResource('/intensive-care-applications', \App\Http\Controllers\API\IntensiveCareApplicationController::class);
+
+// Get Doctors
+Route::get('doctors', function (Request $request) {
+    $query = Doctor::query();
+
+    if ($request->has('city'))
+    {
+        $query->where('city', $request->input('city'));
+    }
+
+    if ($request->has('specialization'))
+    {
+        $query->where('specialization', $request->input('specialization'));
+    }
+
+    if ($request->has('available'))
+    {
+        $query->where('work_days', 'like', '%'.$request->input('available').'%');
+    }
+
+    if ($request->has('fees'))
+    {
+        $query->where('clinic_fees', '<=', $request->input('fees'));
+    }
+    $res = $query->paginate(5);
+
+    return $res;
+});
