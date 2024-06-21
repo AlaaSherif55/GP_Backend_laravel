2705bfaed8a0eddc6eba42086ce15b106ffe63b4
hadeer-khaled <hadeer.awad02@eng-st.cu.edu.eg>
2024-06-21 13:01:19 +0300

    add rate colm to nurse

---
+++ app/Http/Controllers/API/NurseController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Nurse;
+use Illuminate\Http\Request;
+
+class NurseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Nurse $nurse)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Nurse $nurse)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Nurse $nurse)
+    {
+        //
+    }
+}
